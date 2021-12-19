8c455c64569d91c4a53778fdef2b18f263e646bc
Denis Chebotarev <6002940+snedi@users.noreply.github.com>
2021-12-19 22:11:34 +0100

    update

--- tomcat/run.sh
+++ tomcat/run.sh
@@ -1,4 +1,4 @@
-# docker network inspect bridge | jq .[0].Containers | grep IPv4Address
+# docker network inspect bridge | jq .[0].Containers
 
 docker run -d -p 2001:8088 --name openolat1 $OPENOLAT_DOCKER_IMAGE_WEB
 docker run -d -p 2002:8088 --name openolat2 $OPENOLAT_DOCKER_IMAGE_WEB
